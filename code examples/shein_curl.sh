f6532f0f5e5eb53978f7a6530063d95277cf8ff1
AugustOxy <129506779+AugustOxy@users.noreply.github.com>
2023-12-18 12:36:21 +0200

    Adding code examples/shein_curl.sh

---
+++ code examples/shein_curl.sh
@@ -0,0 +1,4 @@
+curl --user user:pass \
+'https://realtime.oxylabs.io/v1/queries' \
+-H "Content-Type: application/json" \
+-d '{"source": "universal", "url": "https://m.shein.com/eur/kids?lang=en&ref=m&rep=dir&ret=meur"}'
